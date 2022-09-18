0252085b121dd047d6b555bf95a57d21b7062d42
jszafran <jszafran.pv@gmail.com>
2022-09-18 15:11:43 +0200

    Render JSON by default on prod.

--- backend/backend/production.py
+++ backend/backend/production.py
@@ -11,3 +11,5 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 ALLOWED_HOSTS = ["srv12.mikr.us"]
 
 DEBUG = False
+
+REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = ("rest_framework.renderers.JSONRenderer",)
