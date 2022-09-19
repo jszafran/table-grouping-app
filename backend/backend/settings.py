6ba7b9d586968f80182710115cbb91e2dd58ff39
jszafran <jszafran.pv@gmail.com>
2022-09-19 19:55:28 +0200

    Middleware for adding delay to request.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "alerts.middleware.RequestSleepMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -134,3 +135,5 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+REQUEST_SLEEP_SECONDS = 1
